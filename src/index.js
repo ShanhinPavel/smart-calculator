235a11bc44db18cafb3ea21a69d609f915fe9586
ShanhinPavel <shanMail@tut.by>
2018-03-13 23:39:20 +0300

    The eleventh task has solved

--- src/index.js
+++ src/index.js
@@ -1,27 +1,50 @@
 class SmartCalculator {
   constructor(initialValue) {
     // your implementation
+    this._initialValue = initialValue;
+    this._expressionValue = String(this._initialValue);
+      }
+
+  toString() {
+    this._expressionValue = eval(this._expressionValue);
+    return this._expressionValue;
+  }
+
+  valueOf() {
+    this._expressionValue = eval(this._expressionValue);
+    return this._expressionValue;
   }
 
   add(number) {
-    // your implementation
-  }
-  
+      this._expressionValue = this._expressionValue + " + " +number;
+      return this;
+    }
+
   subtract(number) {
-    // your implementation
+    this._expressionValue = this._expressionValue + " - " +number;
+    return this;
   }
 
   multiply(number) {
-    // your implementation
+    this._expressionValue = this._expressionValue + " * " +number;
+    return this;
   }
 
   devide(number) {
-    // your implementation
+    this._expressionValue = this._expressionValue + " / " +number;
+    return this;
   }
 
-  pow(number) {
-    // your implementation
+  pow (number) {
+    
+    let array = this._expressionValue.split(" ");
+    let element = array.pop();
+    element = +element;
+    let pow = Math.pow(element, number);
+    array.push(pow);
+    this._expressionValue = array.join(" ");
+    return this;
   }
-}
+  }
 
 module.exports = SmartCalculator;
